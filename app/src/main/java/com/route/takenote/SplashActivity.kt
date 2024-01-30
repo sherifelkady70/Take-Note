fc2f780ea96feeedf358c7d89ceb8c3bfc85c329
sherif Abdelrhman <sellkady038@gmail.com>
2024-01-30 16:01:20 +0200

    layouts names setup

---
+++ app/src/main/java/com/route/takenote/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.route.takenote
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
